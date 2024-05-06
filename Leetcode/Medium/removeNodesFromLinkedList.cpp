575f60dfcbafd49eb7d8eb874ca20f1a5bbbfa74
leesiyuan <lee.si.yuan.20@gmail.com>
2024-05-07 00:57:23 +0800

    Create file

---
+++ Leetcode/Medium/removeNodesFromLinkedList.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if (!head || !head->next) return head;
+        // assume prev has the largest head
+        ListNode* prev = removeNodes(head->next);
+        if (head->val < prev->val) return prev;
+        head->next = prev;
+        return head;
+    }
+};
