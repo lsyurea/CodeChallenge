939b2f5e1fec87f37281ae3925219154753445bd
leesiyuan <lee.si.yuan.20@gmail.com>
2024-05-09 16:15:03 +0800

    Create file

---
+++ Leetcode/Easy/fizzBuzz.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops", "no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+static const auto DPSolver = [](){ 
+    std::ios_base::sync_with_stdio(false); 
+    std::cin.tie(nullptr); 
+    std::cout.tie(nullptr); 
+    return 'c'; 
+}();
+class Solution {
+public:
+    Solution(){
+        DPSolver; 
+    }
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) res.push_back("FizzBuzz");
+            else if (i % 3 == 0) res.push_back("Fizz");
+            else if (i % 5 == 0) res.push_back("Buzz");
+            else res.push_back(to_string(i));
+        }
+        return res;
+    }
+};
